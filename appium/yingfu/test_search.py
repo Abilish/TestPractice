c53e33266d9f3aaaeb744f038b70cb627579fc86
王薇薇 <sfy_mx@163.com>
2022-03-25 21:42:43 +0800

    first submit

---
+++ appium/yingfu/test_search.py
@@ -0,0 +1,14 @@
+from appium import webdriver
+
+desired_caps={
+    "platformName": 'Android',
+    "platformVerdion": '5.1.1',
+    "deviceName": '192.xxx.x.xxx:5555',
+    "appPackage" 'com.android.settings'
+    "appActivity": 'com.android.settings.Settings'
+}
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+
+
+driver.quit()
